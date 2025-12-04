daaf506e06f671ad6de48bc9b0ba78a484774d91
Sandanitin <sandanithin2@gmail.com>
2025-12-04 17:10:01 +0530

    FOOTER

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,7 +88,7 @@ const Footer = () => {
           <div>
             <div className="flex items-center space-x-2 mb-6">
               <img
-                src="public/DC[1].png"
+                src="/DC[1].png"
                 alt="Dec Closets Logo"
                 className="h-10 w-auto"
               />
